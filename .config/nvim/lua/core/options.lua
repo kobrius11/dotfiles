0f4854a32785aac880a239d8f4ca2facc19501c1
kobrius11 <zygimantas.bickus@gmail.com>
2026-01-04 22:10:54 +0200

    zsh xdg

--- .config/nvim/lua/core/options.lua
+++ .config/nvim/lua/core/options.lua
@@ -1,49 +1,55 @@
-vim.opt.cursorline = true
-vim.opt.scrolloff = 15
-vim.opt.list = true
 
-vim.opt.signcolumn = "number"
-vim.opt.mouse = 'a'
+local opt = vim.opt
+
+opt.cursorline = true
+opt.scrolloff = 15
+opt.list = true
+
+opt.signcolumn = "number"
+opt.mouse = 'a'
 
 -- Relative and absolute line numbers combined
-vim.opt.number = true
-vim.opt.relativenumber = true
+opt.number = true
+opt.relativenumber = true
 
 -- Keep signcolumn on by default
-vim.opt.signcolumn = 'yes'
+opt.signcolumn = 'yes'
 
 -- Cursorline
-vim.opt.cursorline = true
+opt.cursorline = true
 
 -- Show whitespace characters
-vim.opt.list = true
-vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
+opt.list = true
+opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
 
 -- Search
-vim.opt.ignorecase = true
-vim.opt.smartcase = true
-vim.opt.hlsearch = false
-vim.opt.incsearch = true
+opt.ignorecase = true
+opt.smartcase = true
+opt.hlsearch = false
+opt.incsearch = true
 
 -- Preview substitutions
-vim.opt.inccommand = 'split'
+opt.inccommand = 'split'
 
 -- Text wrapping
-vim.opt.wrap = true
-vim.opt.breakindent = true
+opt.wrap = true
+opt.breakindent = true
 
 -- Tabstops
-vim.opt.expandtab = true
-vim.opt.tabstop = 2
-vim.opt.softtabstop = 2
-vim.opt.shiftwidth = 2
+opt.expandtab = true
+opt.tabstop = 2
+opt.softtabstop = 2
+opt.shiftwidth = 2
 
 -- Window splitting
-vim.opt.splitright = true
-vim.opt.splitbelow = true
+opt.splitright = true
+opt.splitbelow = true
 
 -- Save undo history
-vim.opt.undofile = true
+opt.undofile = true
 
-vim.opt.updatetime = 50
-vim.opt.colorcolumn = "120"
+opt.updatetime = 50
+opt.colorcolumn = "120"
+
+opt.autoread = true
+
--- .config/zsh/.zprofile
+++ .config/zsh/.zprofile
@@ -5,15 +5,34 @@ export EDITOR="nvim"
 export XDG_CONFIG_HOME="$HOME/.config"
 export XDG_DATA_HOME="$HOME/.local/share"
 export XDG_CACHE_HOME="$HOME/.cache"
-
-# bootstrap .zshrc to ~/.config/zsh/.zshrc, any other zsh config files can also reside here
-export ZDOTDIR="$XDG_CONFIG_HOME/zsh"
+export XDG_STATE_HOME="$HOME/.local/state"
+export XDG_DATA_DIRS="/usr/local/share:/usr/share:$HOME/.local/share"
+export XDG_CONFIG_DIRS="/etc/xdg"
 
 # history files
 export LESSHISTFILE="$XDG_CACHE_HOME/less_history"
 export PYTHON_HISTORY="$XDG_DATA_HOME/python/history"
+export GNUPGHOME="$XDG_DATA_HOME/gnupg"
+export CARGO_HOME="$XDG_DATA_HOME/cargo"
+export RUSTUP_HOME="$XDG_DATA_HOME/rustup"
+export ACKRC="$XDG_CONFIG_HOME/ack/ackrc"
+export ASDF_CONFIG_FILE="${XDG_CONFIG_HOME}/asdf/asdfrc"
+export ASDF_DATA_DIR="${XDG_DATA_HOME}/asdf"
+export AWS_SHARED_CREDENTIALS_FILE="$XDG_CONFIG_HOME/aws/credentials"
+export AWS_CONFIG_FILE="$XDG_CONFIG_HOME/aws/config"
+export BASH_COMPLETION_USER_FILE="$XDG_CONFIG_HOME/bash-completion/bash_completion"
+export DISCORD_USER_DATA_DIR="${XDG_DATA_HOME}"
+export DOCKER_CONFIG="$XDG_CONFIG_HOME/docker"
+export MACHINE_STORAGE_PATH="$XDG_DATA_HOME/docker-machine"
+export FFMPEG_DATADIR="$XDG_CONFIG_HOME/ffmpeg"
+export PSQLRC="$XDG_CONFIG_HOME/pg/psqlrc"
+export PSQL_HISTORY="$XDG_STATE_HOME/psql_history"
+export PGPASSFILE="$XDG_CONFIG_HOME/pg/pgpass"
+export PGSERVICEFILE="$XDG_CONFIG_HOME/pg/pg_service.conf"
+export KUBECONFIG="$XDG_CONFIG_HOME/kube" 
+export KUBECACHEDIR="$XDG_CACHE_HOME/kube"
+
 
 # add scripts to path
 export PATH="$XDG_CONFIG_HOME/scripts:$PATH"
 
-echo "Working.."
