b187cfee6e966e0da019567767f95dbd043dc09d
kobrius11 <zygimantas.bickus@gmail.com>
2026-07-13 21:51:48 +0300

    fuck lazy

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -5,24 +5,14 @@ vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.termguicolors = true
 vim.opt.signcolumn = "yes"
-vim.opt.updatetime = 250
-vim.opt.timeoutlen = 300
-vim.opt.ignorecase = true
-vim.opt.smartcase = true
 vim.opt.expandtab = true
 vim.opt.shiftwidth = 2
 vim.opt.tabstop = 2
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
 
-vim.keymap.set("i", "jj", "<Esc>", {
-  desc = "Exit insert mode",
-})
+vim.keymap.set("i", "jj", "<Esc>")
+vim.keymap.set("n", "<leader>w", "<cmd>write<cr>")
+vim.keymap.set("n", "<leader>q", "<cmd>quit<cr>")
 
-vim.keymap.set("n", "<leader>w", "<cmd>write<cr>", {
-  desc = "Save file",
-})
-
-vim.keymap.set("n", "<leader>q", "<cmd>quit<cr>", {
-  desc = "Quit",
-})
-
-require("config.lazy")
+vim.cmd.colorscheme("habamax")
